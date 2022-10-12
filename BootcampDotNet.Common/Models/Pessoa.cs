c7958b54ead6128a8cb5121535f945227091b28f
Brian Souza <souzabrian61@gmail.com>
2022-10-11 22:42:40 -0300

    bootcamp dotnet

--- BootcampDotNet.Common/Models/Pessoa.cs
+++ BootcampDotNet.Common/Models/Pessoa.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BootcampDotNet.Models
+namespace BootcampDotNet.Common.Models
 {
     public class Pessoa
     {
--- BootcampDotNet/Program.cs
+++ BootcampDotNet/Program.cs
@@ -1,4 +1,15 @@
-﻿using BootcampDotNet.Common;
+﻿using BootcampDotNet.Common.Models;
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+
+for (int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+}
 
 
 
@@ -8,8 +19,12 @@
 
 
 
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.Nome = "Brian";
+// pessoa1.Idade = 26;
+// pessoa1.Apresentar();
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
